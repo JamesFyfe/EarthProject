b658142ed36a0feb350ff8fafd91f0b79fc6972e
JamesFyfe <jamesfyfe17@gmail.com>
2023-06-08 18:41:42 -0700

    multithreading test (probably not worth doing)

--- static/js/earth.js
+++ static/js/earth.js
@@ -1,9 +1,9 @@
-import { Vector3 } from 'three'
-import { Vector2 } from 'three'
+// import { Vector3 } from 'three';
+// import { Vector2 } from 'three';
 // import * as THREE from 'three';
 import data from '../data/heightData4096.json' assert { type: 'json' };
 
-export default class Earth {
+export class Earth {
     constructor(radius, resolution, position) {
         this.radius = radius;
         this.resolution = resolution;
@@ -11,57 +11,90 @@ export default class Earth {
         this.faces = [];
 		
         // normal for each face     front, back, right, left, top, bottom
-        const normals = [new Vector3(0, 0, 1), 
+        const normals = [
+            new Vector3(0, 0, 1), 
             new Vector3(0, 0, -1), 
             new Vector3(1, 0, 0), 
             new Vector3(-1, 0, 0), 
             new Vector3(0, 1, 0), 
-            new Vector3(0, -1, 0)];
+            new Vector3(0, -1, 0)
+        ];
 
-        const axesAndCorner = [[new Vector3(1, 0, 0), new Vector3(0, -1, 0), new Vector3(-1, 1, 1)],
-            [new Vector3(-1, 0, 0), new Vector3(0, -1, 0), new Vector3(1, 1, -1)],
-            [new Vector3(0, 0, -1), new Vector3(0, -1, 0), new Vector3(1, 1, 1)],
-            [new Vector3(0, 0, 1), new Vector3(0, -1, 0), new Vector3(-1, 1, -1)],
-            [new Vector3(0, 0, 1), new Vector3(-1, 0, 0), new Vector3(1, 1, -1)],
-            [new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(-1, -1, -1)]];
+        const axesA = [
+            new Vector3(1, 0, 0),
+            new Vector3(-1, 0, 0),
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, 1),
+            new Vector3(0, 0, 1),
+            new Vector3(0, 0, 1)
+        ];
+
+        const axesB = [
+            new Vector3(0, -1, 0),
+            new Vector3(0, -1, 0),
+            new Vector3(0, -1, 0),
+            new Vector3(0, -1, 0),
+            new Vector3(-1, 0, 0),
+            new Vector3(1, 0, 0)
+        ]
+
+        const cornerPoints = [
+            new Vector3(-1, 1, 1),
+            new Vector3(1, 1, -1),
+            new Vector3(1, 1, 1),
+            new Vector3(-1, 1, -1),
+            new Vector3(1, 1, -1),
+            new Vector3(-1, -1, -1)
+        ]
         
         // calls constructor to make each face
         for(let i=0; i<6; i++) {
-            let face = new PlanetFace(radius, resolution, normals[i], axesAndCorner[i], i);
+            let face = new PlanetFace(radius, resolution, normals[i], axesA[i], axesB[i], cornerPoints[i]);
             this.faces[i] = face;
         }
     }
 }
 
 // builds each face
-class PlanetFace {
-    constructor(radius, resolution, normal, axesAndCorner, face) {
+export class PlanetFace {
+    constructor(radius, resolution, normal, axisA, axisB, cornerPoint, parentLOD) {
+        if(radius == undefined) {
+            return this;
+        }
         this.vertices = [];
         //array storing UV coordinates
         this.uvs = [];
         //face is on z = 0 line
         this.indices = [];
 
+        this.normal = normal;
+        this.axisA = axisA;
+        this.axisB = axisB;
+        this.cornerPoint = cornerPoint;
+        this.centerPos = PlanetFace.cubeToSphere(cornerPoint.clone().add(axisA.clone().multiplyScalar(0.5)).add(axisB.clone().multiplyScalar(0.5)));
+
+        // higher level means vertices span shorter distance
+        this.lodLevel = 1;
+        if(parentLOD != undefined) { this.lodLevel = parentLOD * 2; }
+
         // horizontal and vertical axes of plane
-        const axisA = axesAndCorner[0];
-        const axisB = axesAndCorner[1];
-        const cornerPoint = axesAndCorner[2];
+        // const axisA = axesAndCorner[0];
+        // const axisB = axesAndCorner[1];
+        // const cornerPoint = axesAndCorner[2];
 
         // generate vertices, start at corner point then build along axis a then b
         for(let j=0; j <= resolution; j++) 
         {
             const currentVertexB = axisB.clone();
-            currentVertexB.multiplyScalar((j / resolution) * 2);
+            currentVertexB.multiplyScalar((j / resolution) * 2 / this.lodLevel);
             for(let k=0; k <= resolution; k++)
             {
                 const currentVertexA = axisA.clone();
-                currentVertexA.multiplyScalar((k / resolution) * 2);
+                currentVertexA.multiplyScalar((k / resolution) * 2 / this.lodLevel);
 
 				// add vertexB and vertexA to corner point to get current vertex location
                 let currentVertex = cornerPoint.clone();
                 currentVertex.add(currentVertexA).add(currentVertexB);
-
-                // currentVertex.normalize()
 
 				// get location on sphere and UV
                 currentVertex = PlanetFace.cubeToSphere(currentVertex);
@@ -75,7 +108,7 @@ class PlanetFace {
 				if(x == data.length) { x=0; }
 				if(y == data.length/2) { y=0; }
 
-				let heightMult = 0.02;
+				let heightMult = 0.015;
 				let vertexHeight = radius + data[x][y] / 61826 * heightMult;
 
                 currentVertex.multiplyScalar(vertexHeight);
@@ -95,8 +128,9 @@ class PlanetFace {
         }
 
         let i = 0, resolutionX = resolution;
+        // fixes seams
         if(normal.x == -1 || normal.y == 1 || normal.y == -1) {
-            resolutionX ++;
+            resolutionX++;
         }
         // build triangles from each point
         for(let j=0; j < resolution; j++) 
@@ -118,27 +152,99 @@ class PlanetFace {
     }
 
     static cubeToSphere(p) {
-        const px = p.getComponent(0);
-        const py = p.getComponent(1);
-        const pz = p.getComponent(2);
-    	const x2 = px * px;
-    	const y2 = py * py;
-    	const z2 = pz * pz;
-    	const x = px * Math.sqrt(1 - (y2 + z2) / 2 + (y2 * z2) / 3);
-    	const y = py * Math.sqrt(1 - (z2 + x2) / 2 + (z2 * x2) / 3);
-    	const z = pz * Math.sqrt(1 - (x2 + y2) / 2 + (x2 * y2) / 3);
+    	const x2 = p.x * p.x;
+    	const y2 = p.y * p.y;
+    	const z2 = p.z * p.z;
+    	const x = p.x * Math.sqrt(1 - (y2 + z2) / 2 + (y2 * z2) / 3);
+    	const y = p.y * Math.sqrt(1 - (z2 + x2) / 2 + (z2 * x2) / 3);
+    	const z = p.z * Math.sqrt(1 - (x2 + y2) / 2 + (x2 * y2) / 3);
     	return new Vector3(x, y, z);
     }
 
     // should calculate all before hand and store in array
     static pointOnSphereToUV(p) {
-        // const p = point.clone();
-        // p.normalize();
-        const x = p.getComponent(0);
-        const y = p.getComponent(1);
-        const z = p.getComponent(2);
-        const v = 0.5 + (Math.asin(y) / Math.PI);
-        const u = 0.5 - (Math.atan2(z, x) / (2 * Math.PI));
-        return new Vector2(u, v);
+        const v = 0.5 + (Math.asin(p.y) / Math.PI);
+        const u = 0.5 - (Math.atan2(p.z, p.x) / (2 * Math.PI));
+        return new Vector3(u, v);
+    }
+}
+
+export function jsonToFace(json) {
+    let face = new PlanetFace();
+    face.vertices = json['vertices'];
+    face.uvs = json['uvs'];
+    face.indices = json['indices'];
+    face.normal = json['vertices'];
+    face.axisA = json['axisA'];
+    face.axisB = json['axisV'];
+    face.cornerPoint = json['cornerPoint'];
+    face.centerPos = json['centerPos'];
+    face.lodLevel = json['lodLevel'];
+    return face;
+}
+
+export function splitFace(parentFaceJson, rad, res) {
+    let pNormal = parentFaceJson['normal'];
+    let pAxisA = parentFaceJson['axisA'];
+    let pAxisB = parentFaceJson['axisB'];
+    let pCornerPoint = parentFaceJson['cornerPoint'];
+    let pLOD = parentFaceJson['lodLevel'];
+
+    pNormal = new Vector3(pNormal.x, pNormal.y, pNormal.z);
+    pAxisA = new Vector3(pAxisA.x, pAxisA.y, pAxisA.z);
+    pAxisB = new Vector3(pAxisB.x, pAxisB.y, pAxisB.z);
+    pCornerPoint = new Vector3(pCornerPoint.x, pCornerPoint.y, pCornerPoint.z);
+
+    const face0 = new PlanetFace(rad, res, pNormal, pAxisA, pAxisB, pCornerPoint, pLOD);
+    const face1 = new PlanetFace(rad, res, pNormal, pAxisA, pAxisB, pCornerPoint.clone().add(pAxisA.clone().multiplyScalar(1/pLOD)), pLOD);
+    const face2 = new PlanetFace(rad, res, pNormal, pAxisA, pAxisB, pCornerPoint.clone().add(pAxisB.clone().multiplyScalar(1/pLOD)), pLOD);
+    const face3 = new PlanetFace(rad, res, pNormal, pAxisA, pAxisB, pCornerPoint.clone().add(pAxisA.clone().multiplyScalar(1/pLOD)).add(pAxisB.clone().multiplyScalar(1/pLOD)), pLOD);
+    return [face0, face1, face2, face3];
+}
+
+export function clearFaceData(face) {
+    face.uvs = [];
+    face.indices = [];
+    face.vertices = [];
+}
+
+onmessage = (e) => {
+    console.log("message recieved from main");
+    let startTime = new Date();
+    if(e.origin == '') {
+        let faceData = JSON.parse(e.data[0]);
+        let newFaces = splitFace(faceData, e.data[1], e.data[2]);
+        const parentFaceIndex = e.data[3]
+        // console.log(newFaces);
+        newFaces = JSON.parse(JSON.stringify(newFaces));
+        postMessage([newFaces, parentFaceIndex]);
+        let timeInonmessage = new Date() - startTime;
+        console.log("Time in worker: " + timeInonmessage);
+    }
+}
+
+class Vector3 {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    clone() {
+        return new Vector3(this.x, this.y, this.z);
+    }
+
+    add = function add(v) {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+        return this;
+    }
+
+    multiplyScalar(n) {
+        this.x *= n;
+        this.y *= n;
+        this.z *= n;
+        return this;
     }
 }
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,16 @@
 // import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@0.147.0/examples/jsm/loaders/OBJLoader.js';
 // import { MTLLoader } from 'https://cdn.jsdelivr.net/npm/three@0.147.0/examples/jsm/loaders/MTLLoader.js';
 import * as THREE from 'three';
-import * as BufferGeometryUtils from 'BufferGeometryUtils';
+// import * as BufferGeometryUtils from 'BufferGeometryUtils';
 import { OrbitControls } from 'OrbitControls';
-import Earth from './earth.js';
+import { Earth, jsonToFace, clearFaceData } from './earth.js';
 
-
+const worker = new Worker('./js/earth.js', {type:'module'});
+let workerBusy = false;
 // Loading
 const textureLoader = new THREE.TextureLoader();
-const normalTexture = textureLoader.load('./imgs/normalMap16kFlat.jpg');
-
+// const normalTexture = textureLoader.load('./imgs/normalMap16kFlat.jpg');
+const normalTexture = textureLoader.load('./imgs/output.jpg');
 
 const earthTexture = textureLoader.load('./imgs/ULQColor_edit.jpg');
 // const earthTexture = textureLoader.load('./imgs/color21600.png');
@@ -24,7 +25,7 @@ const scene = new THREE.Scene();
 const earthMat = new THREE.MeshStandardMaterial({
     map: earthTexture,
     normalMap: normalTexture,
-    normalScale: new THREE.Vector2(3, 3),
+    normalScale: new THREE.Vector2(1, 1),
     roughness: 0.4,
     // metalness: 0.1,
 });
@@ -33,38 +34,38 @@ const basicMat = new THREE.MeshBasicMaterial({
     map: earthTexture
 });
 
-const rad = 1, res = 1000;
+const rad = 1, res = 500;
 const earth = new Earth(rad, res, new THREE.Vector3(0, 0, 0));
 const earthGroup = new THREE.Group();
 
-const geometry = []
+const geometries = []
+let meshes = []
 for(let i=0; i<6; i++) {
-    geometry[i] = new THREE.BufferGeometry();
-    geometry[i].setIndex(earth.faces[i].indices);
-    geometry[i].setAttribute( 'position', new THREE.Float32BufferAttribute(earth.faces[i].vertices, 3) );
+    geometries[i] = new THREE.BufferGeometry();
+    geometries[i].setIndex(earth.faces[i].indices);
+    geometries[i].setAttribute( 'position', new THREE.Float32BufferAttribute(earth.faces[i].vertices, 3) );
+    geometries[i].setAttribute( 'uv', new THREE.Float32BufferAttribute(earth.faces[i].uvs, 2, res * res * 6));
+    geometries[i].computeVertexNormals();
 
+    meshes[i] = new THREE.Mesh(geometries[i], earthMat);
+    earthGroup.add(meshes[i]);
 
-    geometry[i].setAttribute( 'uv', new THREE.Float32BufferAttribute(earth.faces[i].uvs, 2, res * res * 6));
-
-    // const wireframe = new THREE.WireframeGeometry( geometry[i] );
+    // const wireframe = new THREE.WireframeGeometry( geometries[i] );
     // const line = new THREE.LineSegments( wireframe );
     // line.material.depthTest = false;
     // line.material.opacity = 0.25;
     // line.material.transparent = true;
     // earthGroup.add(line);
-
 }
 
-let singleGeometry = BufferGeometryUtils.mergeGeometries(geometry);
-singleGeometry.computeVertexNormals();
+// let singleGeometry = BufferGeometryUtils.mergeGeometries(geometry);
+// singleGeometry.computeVertexNormals();
 // singleGeometry.normalizeNormals();
 // let earthJSON = singleGeometry.toJSON();
 // console.log(earthJSON);
 
-const singleMesh = new THREE.Mesh(singleGeometry, earthMat);
-// const singleMesh = new THREE.Mesh(singleGeometry, basicMat);
-earthGroup.add(singleMesh);
-// console.log(earthGroup)
+// const singleMesh = new THREE.Mesh(singleGeometry, earthMat);
+// earthGroup.add(singleMesh);
 
 scene.add(earthGroup);
 
@@ -77,6 +78,9 @@ const dirLight = new THREE.DirectionalLight(0xffffff, 1);
 dirLight.position.set(-.7, -.1, 0.6);
 // dirLight.castShadow = true;
 scene.add(dirLight);
+
+const ambientLight = new THREE.AmbientLight(0x404040, 0.5); // soft white light
+scene.add( ambientLight );
 
 let screenWidth = window.innerWidth;
 let screenHeight = window.innerHeight;
@@ -109,7 +113,7 @@ scene.add(camera);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.maxDistance = 4;
-controls.minDistance = 1.15;
+controls.minDistance = 1.05;
 controls.zoomSpeed = 0.05;
 
 // Renderer
@@ -120,25 +124,89 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(screenWidth, screenHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+function increaseLOD(i) {
+    console.log("increasing LOD of mesh " + i);
+    // clear face data to make json smaller as we dont need that data anymore
+    clearFaceData(earth.faces[i]);
+    let face = JSON.stringify(earth.faces[i]);
+
+    // const newFaces = []
+    // for(let i=0; i<4; i++) {
+    //     newFaces[i] = new PlanetFace();
+    // }
+
+    worker.postMessage([face, rad, res, i]);
+    workerBusy = true;
+    console.log("Message posted to worker");
+}
+
+worker.onmessage = (e) => {
+    console.log("message recieved from worker");
+    workerBusy = false;
+    let startTime = new Date();
+    // console.log(e);
+    const parentIndex = e.data[1];
+    const newFaces = [];
+    for(let k=0; k<4; k++) {
+        newFaces[k] = jsonToFace(e.data[0][k]);
+    }
+    let timeLoadingFaces = new Date() - startTime;
+    console.log("Time loading faces: " + timeLoadingFaces);
+    startTime = new Date();
+    const newGeometries = []
+    const newMeshes = []
+    for(let j=0; j<4; j++) {
+        newGeometries[j] = new THREE.BufferGeometry();
+        newGeometries[j].setIndex(newFaces[j].indices);
+        newGeometries[j].setAttribute( 'position', new THREE.Float32BufferAttribute(newFaces[j].vertices, 3) );
+        newGeometries[j].setAttribute( 'uv', new THREE.Float32BufferAttribute(newFaces[j].uvs, 2, res * res * 6));
+        newGeometries[j].computeVertexNormals();
+
+        newMeshes[j] = new THREE.Mesh(newGeometries[j], earthMat);
+        earthGroup.add(newMeshes[j]);
+    }
+    let timeLoadingMeshes = new Date() - startTime;
+
+    console.log("Time creating meshes: " + timeLoadingMeshes);
+    earth.faces = earth.faces.concat(newFaces);
+    earth.faces.splice(parentIndex);
+    earthGroup.remove(meshes[parentIndex])
+    meshes.splice(parentIndex)
+    meshes = meshes.concat(newMeshes);
+}
+
+function getCameraDist() {
+    return camera.position.distanceTo(earth.position);
+}
+
+function checkIncreaseLOD() {
+    if(getCameraDist() < 1.3 && !workerBusy) {
+        let closest = Infinity;
+        let closeFace = -1;
+        for(let i=0; i<earth.faces.length; i++) {
+            const dist = camera.position.distanceTo(earth.faces[i].centerPos);
+            if(dist < closest) {
+                closest = dist;
+                closeFace = i;
+            }
+        }
+        if(closeFace != -1 && earth.faces[closeFace].lodLevel == 1) {
+            increaseLOD(closeFace)
+        }
+    }
+}
+
 const tick = () =>
 {
-
-
     // Update objects
-    // earthGroup.rotation.y = 1
-    // earthGroup.rotation.y = Math.PI/2
-    // earthGroup.rotation.x = -Math.PI/2
-    earthGroup.rotation.y += 0.001;
+    // earthGroup.rotation.y += 0.001;
     
     // Update Orbital Controls
-    controls.update()
-
+    controls.update();
     // Render
     renderer.render(scene, camera);
-    // console.log(earthGroup.rotation.x)
-
+    checkIncreaseLOD();
     // Call tick again on the next frame
     window.requestAnimationFrame(tick);
 }
-
 tick();
